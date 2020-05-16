4148dbe206911913af4aeadf9661f7157a98b797
juanGM10 <luisguerra.jlgm@gmail.com>
2020-05-15 20:30:45 -0500

    first commit

---
+++ src/app/shared/siderbar/siderbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-siderbar',
+  templateUrl: './siderbar.component.html',
+  styles: []
+})
+export class SiderbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
